c9d1ee34ff94e63bd04ab5f6c3918fb590a02394
radiating-pumpkin0Zed <a2zthisisforearningmoney@gmail.com>
2026-04-05 11:04:31 +0000

    wiring up the sidebar

--- app.py
+++ app.py
@@ -1,20 +1,30 @@
 from textual.app import App, ComposeResult
 from textual.widgets import Header, Footer, ListView, ListItem, Label
 from textual.containers import Horizontal, Vertical
+from textual.widgets import Static
 
 from parser import get_games
 
 class GameLauncher(App):
 
     def compose(self) -> ComposeResult:
+        self.games = get_games()
         yield Header()
         with Horizontal():
             with Vertical(id="sidebar"):
-                for game in get_games():
-                    yield ListItem(Label(game["name"]))
+                yield ListView(
+                        *[LabelItem(Label(g["name"])) for g in self.games]
+                )
             with Vertical(id="detail"):
-                yield Label("Select a game to see detail")
+                yield Static("Select a game to see details", id="detail-text")
         yield Footer()
+
+    def on_list_view_selected(self, event: ListView.Selected) -> None:
+        index = event.list_view.index
+        game = self.games[index]
+        self.query_one("#detail-text", Static).update(
+                f"Name: {game['name']}\nType: {game['type']}\nPath: {game['path']}"
+        )
 
 if __name__ == "__main__":
     app = GameLauncher()
